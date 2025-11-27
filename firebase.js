30cac6a383936b40359168018bf707c70b09ee8f
krishnathakur87272-ux <krishnathakur87272@gmail.com>
2025-11-27 12:31:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-// Replace with your Firebase config
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'REPLACE_APIKEY',
-  authDomain: 'REPLACE_AUTHDOMAIN',
-  projectId: 'REPLACE_PROJECTID',
-  storageBucket: 'REPLACE_STORAGEBUCKET',
-  messagingSenderId: 'REPLACE_MESSAGING_SENDER_ID',
-  appId: 'REPLACE_APPID'
+  apiKey: "AIzaSyAc19pKhn46cgBdTd3r6KwW46EljyWo1Z0",
+  authDomain: "jeevanwork-f80a3.firebaseapp.com",
+  projectId: "jeevanwork-f80a3",
+  storageBucket: "jeevanwork-f80a3.firebasestorage.app",
+  messagingSenderId: "328869735000",
+  appId: "1:328869735000:web:4ab1a6880157b70b8e782d",
+  measurementId: "G-R7VV66C2MW"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
